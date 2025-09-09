4f11a8a4363c18e4ac0cf1a50f1296f32e6ebca3
pourush kahsyap <pourushkashyap06@gmail.com>
2025-09-09 19:35:03 +0530

    add changes

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -3,8 +3,10 @@ import { axiosInstance } from '../utils/axios.js'
 import toast from 'react-hot-toast'
 import {io} from "socket.io-client"
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/api"
-
+const BASE_URL =
+  import.meta.env.VITE_MODE === "development"
+    ? "http://localhost:5000"
+    : "/api";
 export const useAuthStore = create((set,get) =>({
     authUser: null,
     isSigningUp: false,
