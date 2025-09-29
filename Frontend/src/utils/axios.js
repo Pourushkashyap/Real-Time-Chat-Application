8a2fc5d433486f0e354d95e799a6dced122696f6
pourush kahsyap <pourushkashyap06@gmail.com>
2025-09-29 10:47:06 +0530

    dfhggfh

--- Frontend/src/utils/axios.js
+++ Frontend/src/utils/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.DEV
+  ? "http://localhost:5000/api" // local dev
+  : "https://real-time-chat-application-6cwv.onrender.com/api"; // render prod
 
-export const axiosInstance=axios.create({
-    baseURL: import .meta.env.MODE === "development" ? "http://localhost:5000/api": "/api",
-    withCredentials:true,
-
-})
+export const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
