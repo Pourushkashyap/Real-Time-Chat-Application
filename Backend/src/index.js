d5fc4b07e69087a2b0bf4ac8bec61a0f180b130d
pourush kahsyap <pourushkashyap06@gmail.com>
2025-11-29 21:01:22 +0530

    add changes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -17,7 +17,7 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "https://devgroup-xjzm.onrender.com",
-    "https://your-frontend-url.vercel.app"
+    "https://real-time-chat-application.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
